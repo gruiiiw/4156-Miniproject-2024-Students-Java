b1e013d5993e66e5559d4c512769c1aa4b26f526
Grace Dong <107224535+gruiiiw@users.noreply.github.com>
2024-09-13 19:23:21 -0400

    Final bug fixes (#3)
    * Commenting out bugs and adding in unit tests for Course class
    
    * Added department unit tests, added bugs, citations and honesty
    
    * More unit tests and bug fixes
    
    * Fixed bugs, added more unit tests
    
    * Style and pmd fixes, writing unit tests for MyFileDatabase
    
    * Final unit tests for more coverage

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/MyFileDatabaseTests.java
@@ -1,10 +1,41 @@
 package dev.coms4156.project.individualproject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains unit tests for the MyFileDatabase class.
+ */
 public class MyFileDatabaseTests {
 
-  
-  /** The test course instance used for testing. */
-  public static Department testDepartment;
-  
+  private MyFileDatabase testDatabase;
+  private Department testDepartment;
+
+  /** Sets up database for testing.  */
+  @BeforeEach
+  public void setUpTestDatabase() {
+    Map<String, Department> testDepartmentMapping = new HashMap<>();
+    Map<String, Course> courseMapping = new HashMap<>();
+    testDepartment = new Department("COMS", courseMapping, "Luca Carloni", 1);
+    Course coms3251 = new Course("Tony Dear", "402 CHANDLER", "1:10-3:40", 125);
+    courseMapping.put("COMS3251", coms3251);
+
+    testDepartmentMapping.put("COMS", testDepartment);    
+    testDatabase = new MyFileDatabase(1, "./data.txt");
+    testDatabase.setMapping(testDepartmentMapping);
+  }
+
+  @Test
+  public void testToString() {
+    String expected = "For the COMS department: \n"
+                      +
+                      "COMS COMS3251: \n"
+                      + 
+                      "Instructor: Tony Dear; Location: 402 CHANDLER; Time: 1:10-3:40\n";
+    assertEquals(expected, testDatabase.toString());
+  }  
 }
